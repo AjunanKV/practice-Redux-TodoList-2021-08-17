fda4e7c528f6c6abd129ea8b1ed2b82b9ba5b59d
ajunake <kees.vincent.ajunan@oocl.com>
2021-08-19 23:19:01 +0800

    [Todo-practice][Kevin]feat: add modal and implementation to edit text

--- src/component/TodoItem.js
+++ src/component/TodoItem.js
@@ -7,8 +7,10 @@ import {RemoveTodo} from "../common/reducers/todoSlice";
 import "../styles/TodoItem.css";
 import {removeTodos} from "../api/todosApi";
 import {updateTodos} from "../api/todosApi";
-import { CloseCircleFilled } from '@ant-design/icons';
-import { Tooltip, Button,Popconfirm } from 'antd';
+import { CloseCircleFilled,FormOutlined } from '@ant-design/icons';
+import { Tooltip, Button,Popconfirm,Modal,Input } from 'antd';
+import { useState } from 'react';
+
 function TodoItem(props){
     const todo  = useSelector(state => selectTodoById(state,props.id));
     const dispatch = useDispatch();
@@ -16,6 +18,34 @@ function TodoItem(props){
     const [visible, setVisible] = React.useState(false);
     const [confirmLoading] = React.useState(false);
     const propmptText = <span>Please double click to toogle</span>;
+    const [modalText, setModalText] = useState(todo.text)
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+
+    const showModal = () => {
+        setModalText(todo.text)
+        setIsModalVisible(true);
+      };
+
+      const handleModalCancel = () => {
+        setIsModalVisible(false);
+         };
+
+      function ModifyToDo() {
+        if (modalText !== '') {
+            updateTodos(todo.id, {text: modalText})
+            .then((response) => {
+                dispatch(ToogleTodo(response.data))
+            })
+            setIsModalVisible(false);
+        } else {
+            handleModalCancel()
+        }
+      
+        
+     
+
+    }
 
     const showPopconfirm = () => {
         setVisible(true);
@@ -29,6 +59,9 @@ function TodoItem(props){
         });
         
     };
+    function HandleModify(e) {
+        setModalText(e.target.value)
+    } 
 
     function handleRemove(event){
         removeTodos(props.id).then((response)=>{
@@ -49,6 +82,13 @@ function TodoItem(props){
         onCancel={handleCancel}>
             <CloseCircleFilled className = "RemoveButton" onClick={showPopconfirm}></CloseCircleFilled>
             </Popconfirm>
+
+            <FormOutlined className = "RemoveButton" onClick={showModal}/>
+
+            <Modal title="Edit to-do" visible={isModalVisible} onOk={ModifyToDo} onCancel={handleModalCancel}>
+            <Input value={modalText} onChange={HandleModify}></Input>
+            </Modal>
+
         </div>
         </div>
     )
